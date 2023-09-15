d6e6dc3774536a6ff3bcaaf287335698b323b5db
GOLDER303 <szymonstepien@interia.pl>
2023-09-15 17:50:58 +0200

    Add logic for getting the price for a specific game

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log("Hello World!")
+import axios from "axios"
+import * as cheerio from "cheerio"
+import { exit } from "process"
+import * as readline from "readline/promises"
+
+const getPrice = async (gameName: string) => {
+    const response = await axios.get(`https://gg.deals/game/${gameName}`)
+    const data = response.data
+
+    const $ = cheerio.load(data)
+
+    const prices = $("div.header-game-prices-content.active")
+        .find(".game-info-price-col span.price-inner.numeric")
+        .map((_, element) => $(element).text())
+        .get()
+
+    return prices
+}
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+
+const main = async () => {
+    const gameName = await rl.question("What game do you want to check the price? ")
+
+    const [officialPrice, keyshopsPrice] = await getPrice(gameName)
+
+    console.log(`Official: ${officialPrice}`)
+    console.log(`Keyshops: ${keyshopsPrice}`)
+
+    exit()
+}
+
+main()
