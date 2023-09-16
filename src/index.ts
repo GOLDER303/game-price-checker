dabc13dfe0ffac29b1f07d2e8586297b4479e518
GOLDER303 <szymonstepien@interia.pl>
2023-09-16 11:41:47 +0200

    Add input file option to start command

--- src/index.ts
+++ src/index.ts
@@ -114,10 +114,31 @@ const handleInputFile = async (inputFilePath: string) => {
 }
 
 program
-    .arguments("<gameName>")
+    .option("-g, --game-name <gameName>")
+    .option("-i, --input-file-path <inputFilePath>")
     .description("Specify a game name")
-    .action((gameName) => {
-        printSingleGamePrice(gameName)
-    })
 
 program.parse(process.argv)
+
+if (process.argv.length < 3) {
+    program.help()
+}
+
+const options = program.opts<{
+    gameName: string | undefined
+    inputFilePath: string | undefined
+}>()
+
+if (options.gameName && options.inputFilePath) {
+    console.error("Error: You cannot use both -g and -i options at the same time.")
+    exit(1)
+} else if (!options.gameName && !options.inputFilePath) {
+    console.error("Error: You must specify either -g or -i option.")
+    exit(1)
+}
+
+if (options.gameName) {
+    printSingleGamePrice(options.gameName)
+} else if (options.inputFilePath) {
+    handleInputFile(options.inputFilePath)
+}
