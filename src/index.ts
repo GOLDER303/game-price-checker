87005efb0de234cea330164b04693d3e005f2af5
GOLDER303 <szymonstepien@interia.pl>
2023-09-16 11:34:14 +0200

    Add axios error handling

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import axios from "axios"
+import axios, { AxiosError, AxiosResponse } from "axios"
 import * as cheerio from "cheerio"
 import { program } from "commander"
 import { exit } from "process"
@@ -21,7 +21,24 @@ const normalizeGameName = (input: string): string => {
 
 const getGamePrice = async (gameName: string): Promise<[string, string] | null> => {
     const normalizedGameName = normalizeGameName(gameName)
-    const response = await axios.get(`https://gg.deals/game/${normalizedGameName}`)
+
+    let response: AxiosResponse
+    try {
+        response = await axios.get(`https://gg.deals/game/${normalizedGameName}`)
+    } catch (err) {
+        const error = err as AxiosError
+
+        if (error.status === 404) {
+            console.error(`Game "${normalizedGameName}" not found`)
+        } else if (error.response && error.response.status === 404) {
+            console.error(`Game "${normalizedGameName}" not found`)
+        } else {
+            console.error(`Error while fetching game data. Game: "${normalizedGameName}"`)
+        }
+
+        return null
+    }
+
     const data = response.data
 
     const $ = cheerio.load(data)
