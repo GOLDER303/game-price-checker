69abc16ded72c563a94a5268054db95d587e35bc
GOLDER303 <szymonstepien@interia.pl>
2025-07-04 20:54:07 +0200

    Add headers to axios request to avoid 403 error

--- src/data-fetching.ts
+++ src/data-fetching.ts
@@ -12,7 +12,15 @@ export const getGamePrice = async (gameName: string): Promise<[string, string] |
 
     let response: AxiosResponse
     try {
-        response = await axios.get(`https://gg.deals/game/${normalizedGameName}`)
+        response = await axios.get(`https://gg.deals/game/${normalizedGameName}`, {
+            headers: {
+                "User-Agent":
+                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/114.0.0.0 Safari/537.36",
+                Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                "Accept-Language": "en-US,en;q=0.5",
+                Connection: "keep-alive",
+            },
+        })
     } catch (err) {
         const error = err as AxiosError
 
